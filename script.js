5a045916b3aa615f5604b810ed1cb05c79db6110
Nikko <117736564+nikkoboy32@users.noreply.github.com>
2025-07-17 22:54:16 +0800

    almost finish bruhhhhhh

--- script.js
+++ script.js
@@ -1,4 +1,24 @@
 $(document).ready(function(){
+  windowWidth = $(window).width()
+
+  $(".burger-btn").on("click", function() {
+   $("header").css({
+    transform:"translateX(0)",
+   })
+  })
+
+    $(".close-btn").on("click", function() {
+   $("header").css({
+    transform:"translateX(-290px)",
+   })
+  })
+
+ $(window).on("resize", function () {
+    if (windowWidth > 1024) {
+      $("header").css({ transform: "translateX(0)" }); 
+      
+    }
+  });
 
  $('.owl-carousel').owlCarousel({
     loop:true,
@@ -13,14 +33,70 @@ $(document).ready(function(){
             items:1
         },
         600:{
-            items:2
+            items:1
         },
-        1000:{
+        1024:{
             items:2
         }
     }
 })
 
 
+  const shuriken = $("#shuriken")
+
+  gsap.timeline({ repeat: -1 })
+    .to(shuriken, {
+      rotate: "+=360",
+      duration: 1.5,
+      ease: "power1.inOut"
+    })
+    .to(shuriken, {
+      duration: 1, // pause for 1 second
+    });
+
+     gsap.registerPlugin(TextPlugin);
+
+  const roles = [
+    "Front-End Developer",
+    "WordPress Developer",
+    "Website Designer",
+    "Website Developer"
+  ];
+
+  let index = 0;
+
+  function rotateText() {
+    gsap.to("#role", {
+      duration: 1,
+      text: roles[index],
+      ease: "power2.inOut",
+      onComplete: () => {
+        index = (index + 1) % roles.length;
+        setTimeout(rotateText, 1500); // Pause before next word
+      }
+    });
+  }
+
+  rotateText();
+
+  gsap.to(".logo-con h2 span, #footer h3 span" , {
+  scale: 1,
+  color: "#E17564",
+  repeat: -1,
+  yoyo: true,
+  ease: "power2.inOut",
+  duration: 2
+});
+
+ let flipped = false;
+
+  setInterval(() => {
+    gsap.to(".v-flip", {
+      rotationY: flipped ? 0 : 180,
+      duration: 0.6,
+      ease: "power2.inOut"
+    });
+    flipped = !flipped;
+  }, 2000);
 
 }); 
